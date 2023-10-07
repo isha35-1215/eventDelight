125cd701b97f307b1a69072c07f4f4812c39779f
Nishat <123876007+isha35-1215@users.noreply.github.com>
2023-10-07 19:53:35 +0600

    firebase setup done

--- src/firbase/firebase.config.js
+++ src/firbase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCi9_D9dYondKxiY8FXmQPJ7X3Ng86XYPw",
-  authDomain: "dragon-news-auth-946db.firebaseapp.com",
-  projectId: "dragon-news-auth-946db",
-  storageBucket: "dragon-news-auth-946db.appspot.com",
-  messagingSenderId: "331743038104",
-  appId: "1:331743038104:web:08af9d2d68e440aaac9eed"
+  apiKey: "AIzaSyB7w63j13pMNFVmZqhgsuhSjNZM6VPLiIg",
+  authDomain: "event-management-5f92f.firebaseapp.com",
+  projectId: "event-management-5f92f",
+  storageBucket: "event-management-5f92f.appspot.com",
+  messagingSenderId: "606668923256",
+  appId: "1:606668923256:web:6d001a99926a33c14f288d"
 };
 
 // Initialize Firebase
